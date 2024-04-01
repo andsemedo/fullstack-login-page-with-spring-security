28de32fbd4c22cb8c3bb3ec6c3607822643bc56c
Anderson Semedo <andsemedo023@gmail.com>
2024-04-01 19:29:33 -0100

    backend finalizado

--- src/main/java/com/andsemedodev/loginauthapi/repositories/UserRepository.java
+++ src/main/java/com/andsemedodev/loginauthapi/repositories/UserRepository.java
@@ -3,5 +3,8 @@ package com.andsemedodev.loginauthapi.repositories;
 import com.andsemedodev.loginauthapi.domain.user.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface UserRepository extends JpaRepository<User, String> {
+    Optional<User> findByEmail(String loginEmail);
 }
